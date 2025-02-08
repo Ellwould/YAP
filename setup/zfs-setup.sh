9584a46c39457fddd038cd47b91cb916b4e29e96
Elliot Michael Keavney <103308993+Ellwould@users.noreply.github.com>
2025-02-08 16:50:33 +0000

    Update zfs-setup.sh - check user is root
    check user is root

--- setup/zfs-setup.sh
+++ setup/zfs-setup.sh
@@ -1,4 +1,18 @@
 #!/usr/local/bin/bash
 
+######################################################################
+
+# Check if user is root otherwise exit
+
+if [ "$EUID" -ne 0 ]
+then
+  printf "\nPlease run as root\n\n";
+  exit;
+fi;
+
+cd /root;
+
+######################################################################
+
 # Delete any that are not needed (47 in total)
 
